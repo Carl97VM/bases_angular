c1e9deb4f042e4263be50e5e51da604c5cf6cdd8
Carl97VM <barcacarlosv@gmail.com>
2023-08-22 17:58:08 -0400

    all for hero component

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class HeroComponent {
 
+  public name: string = 'Iron Man';
+  public age: number = 45;
+
+  get hobbieName(): string {
+    return this.name.toUpperCase();
+  }
+
+  reset(): void {
+    this.name = 'IronMan';
+    this.age = 45;
+    // document.querySelectorAll('h1')?.forEach(element =>{
+    //   element.innerHTML = '<h1>Desde Angular</h1>'
+    // }) // no es una buena practica forzar estas cosas
+  }
+
+  getHeroDescription(): string {
+    return `${this.name} - ${this.age}`;
+  }
+
+  changeHero():void {
+    this.name = "Spiderman";
+  }
+
+  changeAge(): void {
+    this.age = 25;
+  }
+
+  getvalue():boolean {
+    return true;
+  }
+
 }
