fd247e450b7172f38905bb971ffa11c16a6d118d
Carl97VM <barcacarlosv@gmail.com>
2023-08-25 18:19:28 -0400

    se tiene el codigo d formularios, pero no se añade

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -9,15 +9,23 @@ import { Character } from '../interfaces/character.interface';
 export class MainPageComponent {
   constructor() { }
 
-  public characters: Character[] = [{
-    name: 'Krilin',
-    power: 1000
-  },{
-    name: 'Vegeta',
-    power: 1700
-  },{
-    name: 'Goku',
-    power: 2000
-  }];
+  public characters: Character[] = [
+    {
+      name: 'Krilin',
+      power: 1000
+    }, {
+      name: 'Vegeta',
+      power: 1700
+    }, {
+      name: 'Goku',
+      power: 2000
+    }
+  ];
+  onNewCharacter( character: Character ): void {
+    console.log('Main Page');
+
+    console.log(character);
+
+  }
 
 }
