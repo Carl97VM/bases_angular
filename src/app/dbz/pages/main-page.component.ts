4fcf4101bd97253d82fb3ee10b1f07605800c054
Carl97VM <barcacarlosv@gmail.com>
2023-08-25 16:42:28 -0400

    heredacion a los componentes hijos

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -7,5 +8,16 @@ import { Component } from '@angular/core';
 
 export class MainPageComponent {
   constructor() { }
+
+  public characters: Character[] = [{
+    name: 'Krilin',
+    power: 1000
+  },{
+    name: 'Vegeta',
+    power: 1700
+  },{
+    name: 'Goku',
+    power: 2000
+  }];
 
 }
